0905c9e60ff36b035246270e0fba41f98d2a75a6
ManDude <7569514+ManDude@users.noreply.github.com>
2020-09-04 19:09:14 +0100

    add script

---
+++ Crash 1/NTSC-J/build.bat
@@ -0,0 +1,199 @@
+@echo off
+
+REM "RENAME 'gooc' HERE TO A PATH TO YOUR gooc EXECUTABLE!"
+
+cd C1
+echo ShadC
+gooc -c 1 -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC.nsentry
+gooc -c 1 -l 8 -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-8.nsentry
+gooc -c 1 -l a -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-a.nsentry
+gooc -c 1 -l n -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-n.nsentry
+gooc -c 1 -l r -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-r.nsentry
+gooc -c 1 -l v -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-v.nsentry
+gooc -c 1 -l x -h ShadC.gooh -e bin\%%s.nsentry ShadC.gooc bin\ShadC-x.nsentry
+echo DispC
+gooc -c 1 -h DispC.gooh -e bin\%%s.nsentry DispC.gooc bin\DispC.nsentry
+echo FruiC
+gooc -c 1 -l i -h FruiC.gooh -e bin\%%s.nsentry FruiC.gooc bin\FruiC.nsentry
+echo BoxsC
+gooc -c 1 -r ntsc-j -h BoxsC.gooh -e bin\%%s.nsentry BoxsC.gooc bin\BoxsC.nsentry
+gooc -c 1 -l r -r ntsc-j -h BoxsC.gooh -e bin\%%s.nsentry BoxsC.gooc bin\BoxsC-r.nsentry
+echo DoctC
+gooc -c 1 -l 3 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-3.nsentry
+gooc -c 1 -l 5 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-5.nsentry
+gooc -c 1 -l 6 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-6.nsentry
+gooc -c 1 -l 7 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-7.nsentry
+gooc -c 1 -l 8 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-8.nsentry
+gooc -c 1 -l 9 -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-9.nsentry
+gooc -c 1 -l a -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-a.nsentry
+gooc -c 1 -l c -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-c.nsentry
+gooc -c 1 -l e -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-e.nsentry
+gooc -c 1 -l f -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-f.nsentry
+gooc -c 1 -l h -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-h.nsentry
+gooc -c 1 -l i -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-i.nsentry
+gooc -c 1 -l j -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-j.nsentry
+gooc -c 1 -l k -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-k.nsentry
+gooc -c 1 -l l -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-l.nsentry
+gooc -c 1 -l m -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-m.nsentry
+gooc -c 1 -l n -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-n.nsentry
+gooc -c 1 -l o -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-o.nsentry
+gooc -c 1 -l q -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-q.nsentry
+gooc -c 1 -l r -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-r.nsentry
+gooc -c 1 -l s -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-s.nsentry
+gooc -c 1 -l t -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-t.nsentry
+gooc -c 1 -l w -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-w.nsentry
+gooc -c 1 -l y -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-y.nsentry
+gooc -c 1 -l z -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-z.nsentry
+gooc -c 1 -l E -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-E.nsentry
+gooc -c 1 -l F -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-F.nsentry
+gooc -c 1 -l G -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-G.nsentry
+gooc -c 1 -l K -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-K.nsentry
+gooc -c 1 -l T -r ntsc-j -h DoctC.gooh -e bin\%%s.nsentry DoctC.gooc bin\DoctC-T.nsentry
+echo PoRoC
+gooc -c 1 -h PoRoC.gooh -e bin\%%s.nsentry PoRoC.gooc bin\PoRoC.nsentry
+gooc -c 1 -l 5 -h PoRoC.gooh -e bin\%%s.nsentry PoRoC.gooc bin\PoRoC-5.nsentry
+gooc -c 1 -l U -h PoRoC.gooh -e bin\%%s.nsentry PoRoC.gooc bin\PoRoC-U.nsentry
+echo WateC
+gooc -c 1 -l 3 -h WateC.gooh -e bin\%%s.nsentry WateC.gooc bin\WateC-3.nsentry
+gooc -c 1 -l 7 -h WateC.gooh -e bin\%%s.nsentry WateC.gooc bin\WateC-7.nsentry
+gooc -c 1 -l f -h WateC.gooh -e bin\%%s.nsentry WateC.gooc bin\WateC-f.nsentry
+echo AsciC
+gooc -c 1 -r ntsc-j -h AsciC.gooh -e bin\%%s.nsentry AsciC.gooc bin\AsciC.nsentry
+echo CardC
+gooc -c 1 -r ntsc-j -h CardC.gooh -e bin\%%s.nsentry CardC.gooc bin\CardC.nsentry
+echo WillC
+gooc -c 1 -l h -r ntsc-j -h WillC.gooh -e bin\%%s.nsentry WillC.gooc bin\WillC-h.nsentry
+gooc -c 1 -l i -r ntsc-j -h WillC.gooh -e bin\%%s.nsentry WillC.gooc bin\WillC-i.nsentry
+gooc -c 1 -r ntsc-j -h WillC.gooh -e bin\%%s.nsentry WillC.gooc bin\WillC.nsentry
+echo GamOC
+gooc -c 1 -r ntsc-j -h GamOC.gooh -e bin\%%s.nsentry GamOC.gooc bin\GamOC.nsentry
+echo Opt0C
+gooc -c 1 -h Opt0C.gooh -e bin\%%s.nsentry Opt0C.gooc bin\Opt0C.nsentry
+echo BonoC
+gooc -c 1 -h BonoC.gooh -e bin\%%s.nsentry BonoC.gooc bin\BonoC.nsentry
+echo WarpC
+gooc -c 1 -h WarpC.gooh -e bin\%%s.nsentry WarpC.gooc bin\WarpC.nsentry
+echo MonkC
+gooc -c 1 -h MonkC.gooh -e bin\%%s.nsentry MonkC.gooc bin\MonkC.nsentry
+echo WartC
+gooc -c 1 -h WartC.gooh -e bin\%%s.nsentry WartC.gooc bin\WartC.nsentry
+echo Dog_C
+gooc -c 1 -h Dog_C.gooh -e bin\%%s.nsentry Dog_C.gooc bin\Dog_C.nsentry
+echo PoObC
+gooc -c 1 -h PoObC.gooh -e bin\%%s.nsentry PoObC.gooc bin\PoObC.nsentry
+echo MafiC
+gooc -c 1 -l 3 -h MafiC.gooh -e bin\%%s.nsentry MafiC.gooc bin\MafiC-3.nsentry
+gooc -c 1 -l 8 -h MafiC.gooh -e bin\%%s.nsentry MafiC.gooc bin\MafiC-8.nsentry
+echo PoPlC
+gooc -c 1 -l 3 -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-3.nsentry
+gooc -c 1 -l 5 -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-5.nsentry
+gooc -c 1 -l 6 -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-6.nsentry
+gooc -c 1 -l 7 -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-7.nsentry
+gooc -c 1 -l e -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-e.nsentry
+gooc -c 1 -l j -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-j.nsentry
+gooc -c 1 -l c -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-c.nsentry
+gooc -c 1 -l s -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-s.nsentry
+gooc -c 1 -l y -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc bin\PoPlC-y.nsentry
+gooc -c 1 -l E -h PoPlC.gooh -e bin\%%s.nsentry PoPlC.gooc PoPlC-E..nsentry
+echo BaraC
+gooc -c 1 -h BaraC.gooh -e bin\%%s.nsentry BaraC.gooc bin\BaraC.nsentry
+echo FatsC
+gooc -c 1 -h FatsC.gooh -e bin\%%s.nsentry FatsC.gooc bin\FatsC.nsentry
+echo PinOC
+gooc -c 1 -h PinOC.gooh -e bin\%%s.nsentry PinOC.gooc bin\PinOC.nsentry
+echo PinsC
+gooc -c 1 -h PinsC.gooh -e bin\%%s.nsentry PinsC.gooc bin\PinsC.nsentry
+echo CrabC
+gooc -c 1 -h CrabC.gooh -e bin\%%s.nsentry CrabC.gooc bin\CrabC.nsentry
+echo BeaOC
+gooc -c 1 -h BeaOC.gooh -e bin\%%s.nsentry BeaOC.gooc bin\BeaOC.nsentry
+echo TurtC
+gooc -c 1 -l 9 -h TurtC.gooh -e bin\%%s.nsentry TurtC.gooc bin\TurtC-9.nsentry
+gooc -c 1 -l b -h TurtC.gooh -e bin\%%s.nsentry TurtC.gooc bin\TurtC-b.nsentry
+gooc -c 1 -l i -h TurtC.gooh -e bin\%%s.nsentry TurtC.gooc bin\TurtC-i.nsentry
+gooc -c 1 -l k -h TurtC.gooh -e bin\%%s.nsentry TurtC.gooc bin\TurtC-k.nsentry
+echo CheOC
+gooc -c 1 -h CheOC.gooh -e bin\%%s.nsentry CheOC.gooc bin\CheOC.nsentry
+echo ChefC
+gooc -c 1 -h ChefC.gooh -e bin\%%s.nsentry ChefC.gooc bin\ChefC.nsentry
+echo PlanC
+gooc -c 1 -l c -h PlanC.gooh -e bin\%%s.nsentry PlanC.gooc bin\PlanC-c.nsentry
+gooc -c 1 -l d -h PlanC.gooh -e bin\%%s.nsentry PlanC.gooc bin\PlanC-d.nsentry
+gooc -c 1 -l e -h PlanC.gooh -e bin\%%s.nsentry PlanC.gooc bin\PlanC-e.nsentry
+gooc -c 1 -l i -h PlanC.gooh -e bin\%%s.nsentry PlanC.gooc bin\PlanC-i.nsentry
+gooc -c 1 -l s -h PlanC.gooh -e bin\%%s.nsentry PlanC.gooc bin\PlanC-s.nsentry
+echo PillC
+gooc -c 1 -h PillC.gooh -e bin\%%s.nsentry PillC.gooc bin\PillC.nsentry
+echo JunOC
+gooc -c 1 -l 9 -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-9.nsentry
+gooc -c 1 -l b -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-b.nsentry
+gooc -c 1 -l c -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-c.nsentry
+gooc -c 1 -l d -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-d.nsentry
+gooc -c 1 -l e -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-e.nsentry
+gooc -c 1 -l f -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-f.nsentry
+gooc -c 1 -l P -h JunOC.gooh -e bin\%%s.nsentry JunOC.gooc bin\JunOC-P.nsentry
+echo RivOC
+gooc -c 1 -l f -h RivOC.gooh -e bin\%%s.nsentry RivOC.gooc bin\RivOC-f.nsentry
+gooc -c 1 -l n -h RivOC.gooh -e bin\%%s.nsentry RivOC.gooc bin\RivOC-n.nsentry
+echo VilOC
+gooc -c 1 -h VilOC.gooh -e bin\%%s.nsentry VilOC.gooc bin\VilOC.nsentry
+echo WalOC
+gooc -c 1 -h WalOC.gooh -e bin\%%s.nsentry WalOC.gooc bin\WalOC.nsentry
+echo SheNC
+gooc -c 1 -l h -h SheNC.gooh -e bin\%%s.nsentry SheNC.gooc bin\SheNC-h.nsentry
+gooc -c 1 -l i -h SheNC.gooh -e bin\%%s.nsentry SheNC.gooc bin\SheNC-i.nsentry
+echo BridC
+gooc -c 1 -h BridC.gooh -e bin\%%s.nsentry BridC.gooc bin\BridC.nsentry
+echo GemsC
+gooc -c 1 -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC.nsentry
+gooc -c 1 -l 3 -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-BLUE.nsentry
+gooc -c 1 -l k -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-RED.nsentry
+gooc -c 1 -l f -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-ORANGE.nsentry
+gooc -c 1 -l j -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-PURPLE.nsentry
+gooc -c 1 -l c -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-GREEN.nsentry
+gooc -c 1 -l i -h GemsC.gooh -e bin\%%s.nsentry GemsC.gooc bin\GemsC-YELLOW.nsentry
+echo PoDoC
+gooc -c 1 -h PoDoC.gooh -e bin\%%s.nsentry PoDoC.gooc bin\PoDoC.nsentry
+echo PoREC
+gooc -c 1 -h PoREC.gooh -e bin\%%s.nsentry PoREC.gooc bin\PoREC.nsentry
+echo RRooC
+gooc -c 1 -h RRooC.gooh -e bin\%%s.nsentry RRooC.gooc bin\RRooC.nsentry
+echo RooOC
+gooc -c 1 -h RooOC.gooh -e bin\%%s.nsentry RooOC.gooc bin\RooOC.nsentry
+echo frogC
+gooc -c 1 -h frogC.gooh -e bin\%%s.nsentry frogC.gooc bin\frogC.nsentry
+echo BriOC
+gooc -c 1 -h BriOC.gooh -e bin\%%s.nsentry BriOC.gooc bin\BriOC.nsentry
+echo BrioC
+gooc -c 1 -h BrioC..gooh -e bin\%%s.nsentry BrioC..gooc bin\BrioC..nsentry
+echo SpidC
+gooc -c 1 -h SpidC.gooh -e bin\%%s.nsentry SpidC.gooc bin\SpidC.nsentry
+echo RuiOC
+gooc -c 1 -l s -h RuiOC.gooh -e bin\%%s.nsentry RuiOC.gooc bin\RuiOC-s.nsentry
+gooc -c 1 -l w -h RuiOC.gooh -e bin\%%s.nsentry RuiOC.gooc bin\RuiOC-w.nsentry
+echo CorOC
+gooc -c 1 -h CorOC.gooh -e bin\%%s.nsentry CorOC.gooc bin\CorOC.nsentry
+echo CortC
+gooc -c 1 -h CortC.gooh -e bin\%%s.nsentry CortC.gooc bin\CortC.nsentry
+echo RWaOC
+gooc -c 1 -l w -h RWaOC.gooh -e bin\%%s.nsentry RWaOC.gooc bin\RWaOC-w.nsentry
+gooc -c 1 -l y -h RWaOC.gooh -e bin\%%s.nsentry RWaOC.gooc bin\RWaOC-y.nsentry
+echo LizaC
+gooc -c 1 -h LizaC.gooh -e bin\%%s.nsentry LizaC.gooc bin\LizaC.nsentry
+echo KonOC
+gooc -c 1 -h KonOC.gooh -e bin\%%s.nsentry KonOC.gooc bin\KonOC.nsentry
+echo KongC
+gooc -c 1 -h KongC.gooh -e bin\%%s.nsentry KongC.gooc bin\KongC.nsentry
+echo CasOC
+gooc -c 1 -l y -h CasOC.gooh -e bin\%%s.nsentry CasOC.gooc bin\CasOC-y.nsentry
+gooc -c 1 -l E -h CasOC.gooh -e bin\%%s.nsentry CasOC.gooc bin\CasOC-E.nsentry
+gooc -c 1 -l F -h CasOC.gooh -e bin\%%s.nsentry CasOC.gooc bin\CasOC-F.nsentry
+echo LabAC
+gooc -c 1 -h LabAC.gooh -e bin\%%s.nsentry LabAC.gooc bin\LabAC.nsentry
+echo IsldC
+gooc -c 1 -h IsldC.gooh -e bin\%%s.nsentry IsldC.gooc bin\IsldC.nsentry
+echo WinGC
+gooc -c 1 -r ntsc-j -h WinGC.gooh -e bin\%%s.nsentry WinGC.gooc bin\WinGC.nsentry
+echo Built C1
+robocopy . "C:\Users\DASS\Documents\GitHub\goocdump\Crash 1\NTSC-J" WillC.gooc MonkC.gooc SkunC.gooc FruiC.gooc DispC.gooc DoctC.gooc SnakC.gooc WartC.gooc PoDoC.gooc PoRoC.gooc PoREC.gooc PoPlC.gooc MafiC.gooc Dog_C.gooc PoCoC.gooc PoObC.gooc PinsC.gooc BaraC.gooc FatsC.gooc PinOC.gooc TurtC.gooc ChefC.gooc CheOC.gooc JunOC.gooc BridC.gooc HyeaC.gooc PlanC.gooc CliOC.gooc BeaOC.gooc RivOC.gooc ShadC.gooc KonOC.gooc CrabC.gooc WarpC.gooc WalOC.gooc BoxsC.gooc PillC.gooc frogC.gooc RRooC.gooc SheNC.gooc RooOC.gooc BriOC..gooc BriOC.gooc RuiOC.gooc SpidC.gooc MapOC.gooc KongC.gooc RWaOC.gooc LizaC.gooc Opt0C.gooc CortC.gooc CorOC.gooc VilOC.gooc GamOC.gooc CasOC.gooc LabAC.gooc WateC.gooc BonoC.gooc CardC.gooc GemsC.gooc IsldC.gooc AsciC.gooc WinGC.gooc 
+echo Copied C1
